774127a4be2588346180d7b55eaa0408c90bea5c
Quý <phungxuanquy24721@gmail.com>
2025-12-09 17:38:51 +0700

    Thêm thiết lập bộ định tuyến và tạo các tệp bộ điều khiển và mô hình ban đầu

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
+import router from "./Routers/index.router";
 
 dotenv.config();
 const app = express();
+
+app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
   console.log(`server đag chạy ${process.env.PORT}`);
